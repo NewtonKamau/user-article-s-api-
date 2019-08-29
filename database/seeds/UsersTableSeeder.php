067f90dff87f3656fa64145e54e214ce4e206a4b
newnyc <kamaunewton78@gmail.com>
2019-08-29 13:44:52 +0300

    Added the User seed

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -1,30 +1,31 @@
 <?php
-
+use App\User;
 use Illuminate\Database\Seeder;
-
-
 
 class UsersTableSeeder extends Seeder
 {
-    public function run()
+   public function run()
     {
+        // Let's clear the users table first
         User::truncate();
 
         $faker = \Faker\Factory::create();
-        //hash the password to improve seeder
+
+
         $password = Hash::make('toptal');
 
         User::create([
             'name' => 'Administrator',
             'email' => 'admin@test.com',
-            'password' => '$password'
-         ]);
-        //
-        for ($i =0; $i  < 10; $i ++) {
+            'password' => $password,
+        ]);
+
+        // And now let's generate a few dozen users for our app:
+        for ($i = 0; $i < 10; $i++) {
             User::create([
-             'name' => $faker->name,
-            'email' => $faker->email,
-            'password' => $faker->$password,
+                'name' => $faker->name,
+                'email' => $faker->email,
+                'password' => $password,
             ]);
         }
     }
